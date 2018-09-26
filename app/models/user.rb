67f307ad3cb432f694ec2457303de8e57948ca86
YutaEbisuno <frederika07_bernkastel@yahoo.co.jp>
2018-09-27 05:18:48 +0900

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # コールバック関数。DBにコミット前？でブロック引数を実行
   before_save { email.downcase! }
+
   # バリデーション。必須チェックと長さチェック
   validates :name,
             presence: true,
             length: {maximum: 50}
+
   # バリデーション。必須チェックと長さチェックと正規表現と一意制約（大文字小文字を区別しない）
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,
@@ -12,6 +14,10 @@ class User < ApplicationRecord
             length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: { case_sensitive: false }
+
   # パスワードのハッシュ化
   has_secure_password
+
+  # バリデーション。パスワードの最小文字数
+  validates :password, presence: true, length: { minimum: 6 }
 end
