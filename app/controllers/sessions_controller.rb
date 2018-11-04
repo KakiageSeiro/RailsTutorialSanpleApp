5bb4e5e31481166ab962e3d73cb11a148762ca60
YutaEbisuno <frederika07_bernkastel@yahoo.co.jp>
2018-11-04 15:47:34 +0900

    ログアウト時にcookieのユーザー IDとトークンを削除する処理を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      # セッションにユーザーIDを保存
       log_in user
+
+      # 永続的なセッション情報をDBとCookieへ保存
+      remember user
+
+      # ユーザーページへ遷移
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+
+  # cookieの保存場所
+  attr_accessor :remember_token
+
   # コールバック関数。DBにコミット前？でブロック引数を実行
-  before_save { email.downcase! }
+  before_save {email.downcase!}
 
   # バリデーション。必須チェックと長さチェック
   validates :name,
@@ -13,18 +17,44 @@ class User < ApplicationRecord
             presence: true,
             length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX},
-            uniqueness: { case_sensitive: false }
+            uniqueness: {case_sensitive: false}
 
   # パスワードのハッシュ化
   has_secure_password
 
   # バリデーション。パスワードの最小文字数
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password,
+            presence: true,
+            length: {minimum: 6}
 
   # 渡された文字列のハッシュ値を返す
+  # railsの中のアルゴリズムをパクってきた
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    # トークン生成
+    self.remember_token = User.new_token
+    # トークン文字列のハッシュを生成し、カラム「remember_digest」を更新
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
